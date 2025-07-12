70f5b490903dfa34195c63735ef4d655af64fc5c
Cho Bless <chobless17@gmail.com>
2025-07-12 17:25:43 +0100

    import and const

--- server.js
+++ server.js
@@ -34,3 +34,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+import { config } from "dotenv";
+config();
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
